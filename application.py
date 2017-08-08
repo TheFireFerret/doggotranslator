3bdeb7e5521e7b45c12798e2a7bd642bc2224e93
Sami Glasco <samiglasco@gmail.com>
2017-08-08 02:18:02 -0500

    i hate myself

--- application.py
+++ application.py
@@ -2,19 +2,19 @@ from flask import Flask, request, render_template, url_for, redirect
 from doggobackend import add_translation, get_translations, get_word_type, load_all
 import urllib.parse as urlparse
 
-application = Flask(__name__)
+app = Flask(__name__)
 
-@application.route('/word')
-@application.route('/')
+@app.route('/word')
+@app.route('/')
 def home():
     return render_template('index.html')
 
-@application.errorhandler(404)
+@app.errorhandler(404)
 def page_not_found(e):
     return render_template('404.html'), 404
 
 # TODO add word not found page
-@application.route('/word/<string:word>')
+@app.route('/word/<string:word>')
 def search(word):
 	word_type = get_word_type(word)
 	if word_type is None:
@@ -26,7 +26,7 @@ def search(word):
 	translations = get_translations(word)
 	return render_template('word.html', word=word, word_type=word_type, translations=translations, trans_type=trans_type)
 
-@application.route('/api/add', methods=['POST'])
+@app.route('/api/add', methods=['POST'])
 def new_translation():
 	# print(request.form)
 	inputs = request.form
@@ -38,13 +38,13 @@ def new_translation():
 	add_translation(type_req, inputs["word"], *list(translations[2:]))
 	return redirect(url_for('home'))
 
-@application.route('/all')
+@app.route('/all')
 def view_all():
 	words = load_all()
 	return render_template('list.html', words=words)
 
 # TODO for bot, return words only
-# @application.route('/api/search', methods=['POST'])
+# @app.route('/api/search', methods=['POST'])
 # def search_translation():
 # 	term = request.form['term']
 # 	# print(term)
@@ -54,6 +54,6 @@ def view_all():
 
 if __name__ == "__main__":
     # Setting debug to True enables debug output. This line should be
-    # removed before deploying a production application.
+    # removed before deploying a production app.
     # application.debug = True
-    application.run()
+    app.run()
